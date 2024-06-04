25969604f815db409b8550a7a0d6ba2fd6b8ab6a
201104404 <201104404@lbschools.net>
2024-06-04 11:52:17 -0700

    im so close i can taste it

--- app/src/main/java/com/poly/polyapcsafinalproject23_24/GameAguilonRodney.java
+++ app/src/main/java/com/poly/polyapcsafinalproject23_24/GameAguilonRodney.java
@@ -33,6 +33,7 @@ public class GameAguilonRodney extends GameActivity {
         btn3 = findViewById(R.id.btn_3);
         btn4 = findViewById(R.id.btn_4);
 
+
         tvTitle.setText("Deadly Virus");
         tvSubtitle.setText("High School Edition");
 
@@ -45,6 +46,7 @@ public class GameAguilonRodney extends GameActivity {
         btn1.setVisibility(View.VISIBLE);
         btn2.setVisibility(View.VISIBLE);
         btn3.setVisibility(View.VISIBLE);
+        btn4.setVisibility(View.INVISIBLE);
     }
 
         //create a player object (optional)
@@ -58,7 +60,7 @@ public class GameAguilonRodney extends GameActivity {
             ivStory.setImageResource(R.drawable.im_laborday_title);
             playAudio(R.raw.audio_laborday_bass);
 
-         tvStoryText.setText("Welcome to my game and today we will do our best to survive in this game live or die by choosing a series of choices Sooooooo Where would you like to start?");
+            tvStoryText.setText("Welcome to my game and today we will do our best to survive in this game live or die by choosing a series of choices Sooooooo Where would you like to start?");
             setAllBtnsVisible();
 
             btn1.setText("Go to the store");
@@ -72,13 +74,14 @@ public class GameAguilonRodney extends GameActivity {
             });
             btn2.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v) { goOutSide(); }
+                public void onClick(View v) {goOutSide();}
             });
 
             btn3.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v) { stayHome(); }
+                public void onClick(View v) {stayHome();}
             });
+        }
 
 
 
@@ -100,21 +103,28 @@ public class GameAguilonRodney extends GameActivity {
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                campUp();
-            }
+            public void onClick(View v) {campUp();}
         });
     }
 
 
-    private void lootAndDip()
-    {
+    private void lootAndDip() {
         tvStoryText.setText("You need to get home, what do you do?");
-
+        setAllBtnsVisible();
         btn1.setText("Go home in a car");
         btn2.setText("Go home on foot running");
         btn3.setVisibility(View.INVISIBLE);
-        btn4.setVisibility(View.INVISIBLE);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {goHomeDriving();}
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {goHomeRunning();}
+        });
+    }
 
     private void goHomeDriving()
     {
@@ -126,11 +136,13 @@ public class GameAguilonRodney extends GameActivity {
         tvStoryText.setText("You made it home and made it through the virus since you had food to last you months YOU WIN!!!!");
         end();
     }
+
     private void campUp()
     {
-        tvStoryText.setText("You hear the alarms the infected are coming!!!!!! poeple are breaking in what do you do!!!???");
-        btn1.setText("Fight back to be the only one in")
+        tvStoryText.setText("You hear the alarms the infected are coming!!!!!! people are breaking in what do you do!!!???");
+        btn1.setText("Fight back to be the only one in");
         btn2.setText("Let people through since you nice");
+        btn3.setVisibility(View.INVISIBLE);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -141,8 +153,7 @@ public class GameAguilonRodney extends GameActivity {
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { letThemIn();
-            }
+            public void onClick(View v) {letThemIn();}
         });
     }
     private void fightBack()
@@ -154,17 +165,16 @@ public class GameAguilonRodney extends GameActivity {
     private void letThemIn()
     {
         tvStoryText.setText(" One person was infected and you were infected and died YOU LOSE!!!");
-        defeat();
+        end();
     }
 
     private void goOutSide()
     {
-        tvStoryText.setText (" Plot twist You are infected what will you do, Would you like to do outside?");
-
+        tvStoryText.setText(" Plot twist You are infected what will you do, Would you like to do outside?");
+        setAllBtnsVisible();
         btn1.setText("Infect large group");
         btn2.setText("Infect small group?");
         btn3.setVisibility(View.INVISIBLE);
-        btn4.setVisibility(View.INVISIBLE);
 
 
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -176,147 +186,167 @@ public class GameAguilonRodney extends GameActivity {
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { infectSmallGroup(); }
+            public void onClick(View v) {infectSmallGroup();}
         });
     }
 
 
-
-
-        private void infectLargeGroup()
+    private void infectLargeGroup()
     {
-        System.out.println("So you took the large group to and now start plotting the next move");
-        System.out.println("Take large group to where?");
+        tvStoryText.setText("So you took the large group with you and now start plotting the next move,Take large group to where?");
+        setAllBtnsVisible();
+        btn1.setText("Go to the mall");
+        btn2.setText("Go to the Park");
+        btn3.setVisibility(View.INVISIBLE);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                goToMall();
-            }
+            public void onClick(View v) {goToMall();}
         });
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { goToPark(); }
+            public void onClick(View v) {goToPark();}
         });
     }
 
     private void goToMall()
     {
-        System.out.println("You took a large sum of people and built a large army of infected people and nobody could stop you YOU WINN!!!!!");
-        defeat();
+        tvStoryText.setText("You took a large sum of people and built a large army of infected people and nobody could stop you YOU WINN!!!!!");
+        end();
     }
 
     private void goToPark()
     {
-        System.out.println("Multiple people had spotted your army and alerted the military and your army was shot down YOU LOSE!!!!!");
-        defeat();
+        tvStoryText.setText("Multiple people had spotted your army and alerted the military and your army was shot down YOU LOSE!!!!!");
+        end();
     }
-
 
 
     private void infectSmallGroup()
     {
-        System.out.println("you need to grow stronger where are you going");
-        System.out.println("1. Take small group infected people to city\n2. Take small group infected people to neighborhood");
+        tvStoryText.setText("you need to grow stronger where are you going?\nTake small group infected people to city\nTake small group infected people to neighborhood");
 
-        if (choice == 1)
-        {
-            goToCity();
-        }
-        else if (choice == 2)
-        {
-            goToNeighborhood();
-        }
+        setAllBtnsVisible();
+        btn1.setText("Go to the city");
+        btn2.setText("Go to the Neighborhood");
+        btn3.setVisibility(View.INVISIBLE);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToCity();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {goToNeighborhood();}
+        });
     }
     private void goToCity()
     {
-        System.out.println("The city started to fight back and you ended up dying YOU LOSE!!!!");
-        defeat();
+        tvStoryText.setText("The city started to fight back and you ended up dying YOU LOSE!!!!");
+        end();
     }
 
     private void goToNeighborhood()
     {
-        System.out.println("You ended up sneaking up on unsuspecting people and grew bigger well done YOU WIN!!!!!");
-        defeat();
+        tvStoryText.setText("You ended up sneaking up on unsuspecting people and grew bigger well done YOU WIN!!!!!");
+        end();
     }
 
 
-
-    private void stayHome()
+    private void stayHome ()
     {
-        System.out.println("You are scared and unable to think of what to do so you stay home");
-        System.out.println("1. Set up defence to prevent looters\n2. Use what you have hide in basement");
+        tvStoryText.setText("You are scared and unable to think of what to do so you stay home\nSet up defence to prevent looters or Use what you have hide in basement");
 
-        if (choice == 1)
-        {
-            setUpLooter();
-        }
-        else if (choice == 2)
-        {
-            hideInBasement();
-        }
+        setAllBtnsVisible();
+        btn1.setText("set Up Looter");
+        btn2.setText("Go to the Park");
+        btn3.setVisibility(View.INVISIBLE);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setUpLooter();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {hideInBasement();}
+        });
     }
     private void setUpLooter()
     {
-        System.out.println("Take large group to mall");
+        tvStoryText.setText("you dont have much time\nwill you choose to put video-cams\nwill you set up traps to protect yourself hope to god you live");
+        setAllBtnsVisible();
+        btn1.setText("Put videoCams");
+        btn2.setText("Put Traps");
+        btn3.setVisibility(View.INVISIBLE);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                videoCams();
+            }
+        });
 
-        if (choice == 1)
-        {
-            videoCams();
-        }
-        else if (choice == 2)
-        {
-            putTraps();
-        }
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                putTraps();
+            }
+        });
     }
-    private void videoCams()
+    private void videoCams ()
     {
-        System.out.println("You had no power to do anything but was able to live but had lost everything you owned an won't be able to recover from this You lose!!!!");
-        defeat();
+        tvStoryText.setText("You had no power to do anything but was able to live but had lost everything you owned an won't be able to recover from this You lose!!!!");
+        end();
     }
-    private void putTraps()
+    private void putTraps ()
     {
-        System.out.println("You were able to keep yourself save You defeat!!!!");
-        defeat();
+        tvStoryText.setText("You were able to keep yourself save You live another day!!!!");
+        end();
     }
-    private void hideInBasement()
+    private void hideInBasement ()
     {
-        System.out.println("1.Take food and clothes\n2.Take Water and meds");
+        tvStoryText.setText("you need to stay chilly but theres only two thing you can take food and clothes or water and meds");
+        setAllBtnsVisible();
+        btn1.setText("Take food and clothes");
+        btn2.setText("Take Water and meds");
+        btn3.setVisibility(View.INVISIBLE);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                foodAndClothes();
+            }
+        });
 
-        if (choice == 1)
-        {
-            foodAndClothes();
-        }
-        else if (choice == 2)
-        {
-            waterAndMeds();
-        }
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {waterAndMeds();}
+        });
     }
 
     private void foodAndClothes()
     {
-        System.out.println("Since you had no water you died in three days soo YOU LOSE!!!");
-        defeat();
+        tvStoryText.setText("Since you had no water you died in three days soo YOU LOSE!!!");
+        end();
     }
     private void waterAndMeds()
     {
-        System.out.println("You live since you can only live on water for 3 day had enough water for rescuers to make it to YOU WIN!!!!!!");
-        haveWon();
+        tvStoryText.setText("You live since you can only live on water for 3 day and no need for food up since you can not eat up to three weeks you had enough water for rescuers to make it to YOU WIN!!!!!!");
+        end();
     }
-
-
-
 
 
     private void end()
     {
-        if (isWon)
-        {
-            tvStoryText.setText("You had enough water for rescuers to find and resue you....so YOU WIN!!!!!!\"");
+        if (isWon) {
+            tvStoryText.setText("You finally beat the game so now carry this knolege to the real world buddy goodluck");
 
-            ivStory.setImageResource(R.drawable.im_laborday_miracle);---------------------
-                 -------------------------------------
+            ivStory.setImageResource(R.drawable.im_laborday_miracle);
         }
         else
         {
@@ -325,20 +355,23 @@ public class GameAguilonRodney extends GameActivity {
             tvStoryText.setText(text);
         }
 
-        if (numLives > 0)
-        {
+        if (numLives > 0) {
             btn1.setText("Play again!");
+            btn2.setVisibility(View.INVISIBLE);
+            btn3.setVisibility(View.INVISIBLE);
+            btn4.setVisibility(View.INVISIBLE);
             btn1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     start();
                 }
             });
-        }
-        else
-        {
-            tvStoryText.setText("everything has died. Permenant Game over.");
+        } else {
+            tvStoryText.setText("everything has died. Permanent Game over.");
             btn1.setText("Back to menu");
+            btn2.setVisibility(View.INVISIBLE);
+            btn3.setVisibility(View.INVISIBLE);
+            btn4.setVisibility(View.INVISIBLE);
 
             ivStory.setImageResource(R.drawable.im_laborday_high_school_over);
 
@@ -350,3 +383,4 @@ public class GameAguilonRodney extends GameActivity {
             });
         }
     }
+}
